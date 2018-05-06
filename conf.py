70cf0557f6b051ba3defcb92dfaa4c5f94a5477a
roger77622 <roger77622@gmail.com>
2018-05-06 14:13:37 +0800

    modify conf

--- conf.py
+++ conf.py
@@ -18,15 +18,15 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Roger Wang"  # (translatable)
-BLOG_TITLE = "BLog"  # (translatable)
+BLOG_TITLE = "Blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://ccrogerwang.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
-BLOG_EMAIL = "n.tesla@example.com"
-BLOG_DESCRIPTION = "This is a demo site for Nikola."  # (translatable)
+BLOG_EMAIL = "roger77622@gmail.com"
+BLOG_DESCRIPTION = "Welcome"  # (translatable)
 
 # Nikola is multilingual!
 #
